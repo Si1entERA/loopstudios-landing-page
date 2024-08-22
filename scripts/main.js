3ae4ead5a9c824c64efa861ddacab33e7a08610a
Si1entERA <NeoLegote@outlook.com>
2024-08-22 21:24:32 +0200

    sidebar nav complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,23 @@
 
 const openBtn = document.getElementById('open-menu');
+const closeBtn = document.getElementById('close-menu');
 const sidebar = document.querySelector('.top__sidebar');
+
+const body = document.body;
 
 const openMenu = () => {
   sidebar.classList.add('open');
+  body.classList.add('lock');
+  openBtn.style.display = 'none';
+  closeBtn.style.display = 'block';
+}
+
+const closeMenu = () => {
+  sidebar.classList.remove('open');
+  body.classList.remove('lock');
+  openBtn.style.display = 'block';
+  closeBtn.style.display = 'none';
 }
 
 openBtn.addEventListener('click', openMenu)
+closeBtn.addEventListener('click', closeMenu)
