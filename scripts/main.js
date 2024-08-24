2a9cec7a83896696782d5445a5eff838c6a23245
Si1entERA <NeoLegote@outlook.com>
2024-08-24 23:56:22 +0200

    bottom section semi complete

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,8 @@ const openBtn = document.getElementById('open-menu');
 const closeBtn = document.getElementById('close-menu');
 const sidebar = document.querySelector('.top__sidebar');
 
+const currentYear = document.getElementById('year');
+
 const body = document.body;
 
 const openMenu = () => {
@@ -18,6 +20,11 @@ const closeMenu = () => {
   openBtn.style.display = 'block';
   closeBtn.style.display = 'none';
 }
+
+const dt = new Date();
+const year = dt.getFullYear();
+
+currentYear.innerHTML = year
 
 openBtn.addEventListener('click', openMenu)
 closeBtn.addEventListener('click', closeMenu)
